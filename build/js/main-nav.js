9e709dcc9bad9ae0de2cd24314bf009af7dacc3f
arttiam <71148141+arttiam@users.noreply.github.com>
2021-04-28 23:22:37 +0500

    сборка

---
+++ build/js/main-nav.js
@@ -0,0 +1,1 @@
+let btnHamburger=document.querySelector(".main-nav__toggle"),mainMenu=document.querySelector(".main-nav__list");btnHamburger.classList.remove("main-nav__toggle--closed"),mainMenu.classList.add("main-nav--closed"),btnHamburger.addEventListener("click",(function(){btnHamburger.classList.toggle("main-nav__toggle--closed"),mainMenu.classList.toggle("main-nav--closed")}));
